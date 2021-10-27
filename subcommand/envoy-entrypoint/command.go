6871fc4a4c271aae4f08b8d05c15c92a55aec0ea
Paul Glass <pglass@hashicorp.com>
2021-10-27 15:09:49 -0500

    Tidy up envoy-entrypoint

--- subcommand/envoy-entrypoint/command.go
+++ subcommand/envoy-entrypoint/command.go
@@ -3,22 +3,34 @@
 // This is intended for used as a Docker entrypoint for Envoy:
 // * Run a given command in a subprocess
 // * Forward all signals to the subprocess, except for SIGTERM
-// * Monitor task metadata to terminate Envoy after application containers exit.
+// * Monitor task metadata to terminate Envoy after application container(s) stop
 
 package envoyentrypoint
 
 import (
+	"context"
+	"os"
+	"sync"
+
 	"github.com/hashicorp/go-hclog"
 	"github.com/mitchellh/cli"
 )
 
 type Command struct {
-	UI cli.Ui
+	UI   cli.Ui
+	log  hclog.Logger
+	once sync.Once
 
-	log hclog.Logger
+	sigs       chan os.Signal
+	wg         *sync.WaitGroup
+	ctx        context.Context
+	cancel     context.CancelFunc
+	envoyCmd   *EnvoyCmd
+	appMonitor *AppContainerMonitor
+}
 
-	// unit testing hack
-	pid int
+func (c *Command) init() {
+	c.log = hclog.New(&hclog.LoggerOptions{Name: "consul-ecs"})
 }
 
 func (c *Command) Help() string {
--- subcommand/envoy-entrypoint/command_unix.go
+++ subcommand/envoy-entrypoint/command_unix.go
@@ -6,188 +6,72 @@ package envoyentrypoint
 import (
 	"context"
 	"os"
-	"os/exec"
 	"os/signal"
 	"sync"
 	"syscall"
-	"time"
-
-	"github.com/aws/aws-sdk-go/service/ecs"
-	"github.com/hashicorp/consul-ecs/awsutil"
-	"github.com/hashicorp/go-hclog"
 )
 
 func (c *Command) Run(args []string) int {
-	// TODO: Make log level a flag
-	c.log = hclog.New(&hclog.LoggerOptions{
-		Name:  "consul-ecs",
-		Level: hclog.Info,
-	})
+	c.once.Do(c.init)
 
 	if len(args) == 0 {
 		c.UI.Error("command is required")
 		return 1
 	}
 
-	var wg sync.WaitGroup
-	ctx, cancel := context.WithCancel(context.Background())
+	c.sigs = make(chan os.Signal, 1)
+	c.wg = &sync.WaitGroup{}
+	c.ctx, c.cancel = context.WithCancel(context.Background())
+	c.envoyCmd = NewEnvoyCmd(c.ctx, args)
+	c.appMonitor = NewAppContainerMonitor(c.log, c.ctx)
 
-	defer func() {
-		c.log.Debug("shutting down")
-		cancel()
-		wg.Wait()
-	}()
+	return c.realRun()
+}
 
-	cmd := c.makeCommand(ctx, args)
-	exitCodeChan := make(chan int, 1)
-	pidChan := make(chan int, 1)
+func (c *Command) realRun() int {
+	signal.Notify(c.sigs)
+	defer c.cleanup()
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		c.runCommand(cmd, exitCodeChan, pidChan)
-	}()
+	// Run Envoy in the background, cancellable via the context.
+	go c.envoyCmd.Run(c.wg)
 
-	// Wait for the subprocess to start.
-	if pid, ok := <-pidChan; !ok {
-		c.log.Error("could not run command")
-		return -1
-	} else {
-		c.log.Debug("subprocess started", "pid", pid)
-		c.pid = pid // unit testing hack
+	// Wait for Envoy to start.
+	if _, ok := <-c.envoyCmd.PidCh; !ok {
+		c.log.Error("Envoy failed to start")
+		return 1
 	}
-
-	// Forward all signals to the subprocess. Except SIGTERM.
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs)
-	defer signal.Stop(sigs)
-
-	// On SIGTERM, spawn a goroutine that monitors for app containers to exit.
-	// This channel lets us know the application containers have exited.
-	appExitedChan := make(chan bool, 1)
-	var once sync.Once
 
 	for {
 		select {
-		case exitCode, ok := <-exitCodeChan:
+		case exitCode, ok := <-c.envoyCmd.ExitCodeCh:
+			// When the Envoy process exits, we're done.
 			if ok {
 				return exitCode
 			}
 			return -1
-		case sig := <-sigs:
-			if sig == syscall.SIGTERM {
-				// start monitoring task metadata
-				c.log.Info("ignored", "signal", sig)
-				once.Do(func() {
-					wg.Add(1)
-					go func() {
-						defer wg.Done()
-						c.monitorTaskMeta(ctx, appExitedChan)
-					}()
-				})
-			} else if sig == syscall.SIGCHLD || sig == syscall.SIGURG {
-				// do not forward
-			} else {
-				if err := cmd.Process.Signal(sig); err != nil {
-					c.log.Warn("forwarding signal", "err", err.Error())
-				}
-			}
-		case <-appExitedChan:
-			return cmd.ProcessState.ExitCode()
+		case sig := <-c.sigs:
+			c.handleSignal(sig)
+		case <-c.appMonitor.Done():
+			// When the application containers stop (after SIGTERM), we're done.
+			return 0
 		}
 	}
 }
 
-func (c *Command) makeCommand(ctx context.Context, args []string) *exec.Cmd {
-	// CommandContext allows cancelling the command.
-	// When cancelled, the process is sent a SIGKILL and is not waited on.
-	cmd := exec.CommandContext(ctx, args[0], args[1:]...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	// Use a process group that we can signal for cleanup.
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Setpgid: true,
-	}
-	return cmd
-}
-
-// runCommand Runs the given command, and sends the exit code and process id on the given channels.
-func (c *Command) runCommand(cmd *exec.Cmd, exitCodeCh chan int, pidCh chan int) {
-	defer close(exitCodeCh)
-	defer close(pidCh)
-
-	c.log.Debug("starting subprocess", "path", cmd.Path, "args", cmd.Args)
-	if err := cmd.Start(); err != nil {
-		c.log.Debug("cmd.Start() returned error", "err", err.Error())
-		// Channels are closed in defers to indicate that the command did not start.
-		return
-	} else {
-		pidCh <- cmd.Process.Pid
-	}
-
-	if err := cmd.Wait(); err != nil {
-		c.log.Debug("cmd.Wait() returned error", "err", err.Error())
-		exitCodeCh <- cmd.ProcessState.ExitCode()
-	} else {
-		exitCodeCh <- 0
-	}
-	// Kill the process group to try to clean up leftover sub-processes.
-	err := syscall.Kill(-cmd.Process.Pid, syscall.SIGTERM)
-	if err != nil {
-		c.log.Debug("killing process group", "err", err.Error())
+func (c *Command) handleSignal(sig os.Signal) {
+	if sig == syscall.SIGTERM {
+		c.log.Info("received", "signal", sig)
+		go c.appMonitor.Run(c.wg)
+	} else if sig == syscall.SIGCHLD || sig == syscall.SIGURG {
+		// do not forward
+	} else if err := c.envoyCmd.Process.Signal(sig); err != nil {
+		c.log.Warn("forwarding signal", "err", err.Error())
 	}
 }
 
-func (c *Command) monitorTaskMeta(ctx context.Context, done chan bool) {
-	defer close(done)
-
-	// Poll task metadata to wait for application containers to exit.
-	nonAppContainers := []string{"consul-client", "sidecar-proxy", "health-sync", "consul-ecs-mesh-init"}
-	isAppContainer := func(container awsutil.ECSTaskMetaContainer) bool {
-		for _, ignoreName := range nonAppContainers {
-			if container.Name == ignoreName {
-				return false
-			}
-		}
-		return true
-	}
-	isStopped := func(container awsutil.ECSTaskMetaContainer) bool {
-		// Check DesiredStatus is stopped to ensure we're in Task shutdown.
-		return container.DesiredStatus == ecs.DesiredStatusStopped &&
-			container.KnownStatus == ecs.DesiredStatusStopped
-	}
-
-	c.log.Info("waiting for application container(s) to stop")
-	for {
-		select {
-		case <-ctx.Done():
-			// Allow cancelling this loop.
-			return
-		case <-time.After(1 * time.Second):
-			taskMeta, err := awsutil.ECSTaskMetadata()
-			if err != nil {
-				c.log.Error("fetching task metadata", "err", err.Error())
-				break // escape this case of the select
-			}
-
-			doneWaiting := true
-			for _, container := range taskMeta.Containers {
-				if !isAppContainer(container) {
-					continue
-				}
-
-				if !isStopped(container) {
-					c.log.Debug("app container not yet stopped", "name", container.Name, "status", container.KnownStatus)
-					doneWaiting = false
-				}
-			}
-
-			if doneWaiting {
-				c.log.Info("application container(s) have stopped, terminating envoy")
-				done <- true
-				return
-			}
-		}
-	}
+func (c *Command) cleanup() {
+	signal.Stop(c.sigs)
+	// Cancel the command and background goroutines.
+	c.cancel()
+	c.wg.Wait()
 }

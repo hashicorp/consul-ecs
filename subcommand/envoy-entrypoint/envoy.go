511e18814040f10e5d750bbdf12fc68c821efed6
Paul Glass <pglass@hashicorp.com>
2021-10-29 14:20:09 -0500

    Error handling in envoy-entrypoint

--- subcommand/envoy-entrypoint/envoy.go
+++ subcommand/envoy-entrypoint/envoy.go
@@ -7,16 +7,19 @@ import (
 	"os"
 	"os/exec"
 	"syscall"
+
+	"github.com/hashicorp/go-hclog"
 )
 
 type EnvoyCmd struct {
 	*exec.Cmd
 
+	log       hclog.Logger
 	doneCh    chan struct{}
 	startedCh chan struct{}
 }
 
-func NewEnvoyCmd(args []string) *EnvoyCmd {
+func NewEnvoyCmd(log hclog.Logger, args []string) *EnvoyCmd {
 	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -28,6 +31,7 @@ func NewEnvoyCmd(args []string) *EnvoyCmd {
 
 	return &EnvoyCmd{
 		Cmd:       cmd,
+		log:       log,
 		doneCh:    make(chan struct{}, 1),
 		startedCh: make(chan struct{}, 1),
 	}
@@ -40,16 +44,19 @@ func (e *EnvoyCmd) Run() {
 	defer close(e.startedCh)
 
 	if err := e.Cmd.Start(); err != nil {
+		e.log.Error("starting Envoy process", "error", err.Error())
 		// Closed channels (in defers) indicate the command failed to start.
 		return
 	}
 	e.startedCh <- struct{}{}
 
-	_ = e.Cmd.Wait()
+	if err := e.Cmd.Wait(); err != nil {
+		if _, ok := err.(*exec.ExitError); !ok {
+			// Do not log if it is only a non-zero exit code.
+			e.log.Error("waiting for Envoy process to finish", "error", err.Error())
+		}
+	}
 	e.doneCh <- struct{}{}
-
-	// Signal the process group to exit, to try to clean up subprocesses.
-	_ = syscall.Kill(-e.Cmd.Process.Pid, syscall.SIGTERM)
 }
 
 func (e *EnvoyCmd) Started() chan struct{} {

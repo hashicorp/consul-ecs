834803a71529a3139f7ec59e6b8aea29bc5402dc
Paul Glass <pglass@hashicorp.com>
2022-11-17 15:54:09 -0600

    Enable CRT for 0.4.x (#120)
    * Copy CRT config from main and update for 0.4.x
    * Remove non-CRT stuff: ./.circleci and ./build-support
    * Backport: Extract unit test utils into testutils package (#70)

---
+++ .release/release-metadata.hcl
@@ -0,0 +1,6 @@
+url_docker_registry_dockerhub = "https://hub.docker.com/r/hashicorp/consul-ecs"
+url_docker_registry_ecr = "https://gallery.ecr.aws/hashicorp/consul-ecs"
+url_license           = "https://github.com/hashicorp/consul-ecs/blob/main/LICENSE"
+url_project_website = "https://www.consul.io/docs/ecs"
+url_source_repository = "https://github.com/hashicorp/consul-ecs"
+url_release_notes = "https://www.consul.io/docs/release-notes"
---
+++ testutil/config.go
@@ -0,0 +1,43 @@
+package testutil
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/consul-ecs/config"
+	"github.com/stretchr/testify/require"
+)
+
+// TempDir creates a temporary directory. A test cleanup removes the directory
+// and its contents.
+func TempDir(t *testing.T) string {
+	dir, err := ioutil.TempDir("", "")
+	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		err := os.RemoveAll(dir)
+		if err != nil {
+			t.Logf("warning, failed to cleanup temp dir %s - %s", dir, err)
+		}
+	})
+
+	return dir
+}
+
+// SetECSConfigEnvVar the CONSUL_ECS_CONFIG_JSON environment variable
+// to the JSON string of the provided value, with a test cleanup.
+func SetECSConfigEnvVar(t *testing.T, val interface{}) {
+	configBytes, err := json.MarshalIndent(val, "", "  ")
+	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		_ = os.Unsetenv(config.ConfigEnvironmentVariable)
+	})
+
+	err = os.Setenv(config.ConfigEnvironmentVariable, string(configBytes))
+	require.NoError(t, err)
+
+	t.Logf("%s=%s", config.ConfigEnvironmentVariable, os.Getenv(config.ConfigEnvironmentVariable))
+}

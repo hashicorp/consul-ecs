b0548e246d19984107bf5047ed00f675c2086d78
Chris Thain <32781396+cthain@users.noreply.github.com>
2023-03-14 07:07:04 -0700

    Add net-dial subcommand (#135)

---
+++ subcommand/net-dial/command_test.go
@@ -0,0 +1,45 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package netdial
+
+import (
+	"net"
+	"testing"
+
+	"github.com/mitchellh/cli"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNetDial(t *testing.T) {
+	cases := map[string]struct {
+		host   string
+		code   int
+		errStr string
+	}{
+		"success":              {host: "localhost", code: 0},
+		"failure no listener":  {host: "localhost", code: 2},
+		"failure invalid args": {code: 1},
+	}
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			ui := cli.NewMockUi()
+			cmd := Command{UI: ui}
+
+			var args []string
+
+			if c.host != "" {
+				l, err := net.Listen("tcp", c.host+":")
+				require.NoError(t, err)
+				args = append(args, l.Addr().String())
+				if c.code != 0 {
+					l.Close()
+				} else {
+					t.Cleanup(func() { l.Close() })
+				}
+			}
+
+			require.Equal(t, c.code, cmd.Run(args))
+		})
+	}
+}

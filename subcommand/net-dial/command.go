f1615d8e4dceca02e7f18b5718289e0110ecc3bc
Chris Thain <chris.m.thain@gmail.com>
2023-03-14 07:03:19 -0700

    address review comments

--- subcommand/net-dial/command.go
+++ subcommand/net-dial/command.go
@@ -4,7 +4,6 @@
 package netdial
 
 import (
-	"fmt"
 	"net"
 
 	"github.com/mitchellh/cli"
@@ -15,14 +14,14 @@ type Command struct {
 }
 
 func (c *Command) Run(args []string) int {
-	if len(args) != 2 {
-		c.UI.Error("invalid invocation, expected two positional args: <host> <port>")
+	if len(args) != 1 {
+		c.UI.Error("invalid invocation, expected one positional argument: <host>:<port>")
 		return 1
 	}
 
-	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", args[0], args[1]))
+	conn, err := net.Dial("tcp", args[0])
 	if err != nil {
-		return 1
+		return 2
 	}
 	conn.Close()
 
@@ -30,14 +29,14 @@ func (c *Command) Run(args []string) int {
 }
 
 func (c *Command) Synopsis() string {
-	return "Performs a simple health check by opening a TCP connection to a host and port"
+	return "Checks for a TCP listener on a host"
 }
 
 func (c *Command) Help() string {
-	return `usage: consul-ecs net-dial <host> <port>
+	return `usage: consul-ecs net-dial <host>:<port>
 
 Attempts to open a TCP connection to <host>:<port>.
-It returns with an exit code of 0 if the connection succeeds.
-If the connection fails for any reason, an exit code of 1 is returned.
+An exit code of 0 is returned if the connection succeeds.
+A non zero exit code is returned if the connection fails for any reason.
 `
 }
--- subcommand/net-dial/command_test.go
+++ subcommand/net-dial/command_test.go
@@ -5,7 +5,6 @@ package netdial
 
 import (
 	"net"
-	"strings"
 	"testing"
 
 	"github.com/mitchellh/cli"
@@ -14,23 +13,30 @@ import (
 
 func TestNetDial(t *testing.T) {
 	cases := map[string]struct {
-		code int
+		host   string
+		code   int
+		errStr string
 	}{
-		"success": {code: 0},
-		"failure": {code: 1},
+		"success":              {host: "localhost", code: 0},
+		"failure no listener":  {host: "localhost", code: 2},
+		"failure invalid args": {code: 1},
 	}
 	for name, c := range cases {
 		t.Run(name, func(t *testing.T) {
 			ui := cli.NewMockUi()
 			cmd := Command{UI: ui}
 
-			l, err := net.Listen("tcp", "localhost:")
-			require.NoError(t, err)
-			args := strings.Split(l.Addr().String(), ":")
-			if c.code != 0 {
-				l.Close()
-			} else {
-				t.Cleanup(func() { l.Close() })
+			var args []string
+
+			if c.host != "" {
+				l, err := net.Listen("tcp", c.host+":")
+				require.NoError(t, err)
+				args = append(args, l.Addr().String())
+				if c.code != 0 {
+					l.Close()
+				} else {
+					t.Cleanup(func() { l.Close() })
+				}
 			}
 
 			require.Equal(t, c.code, cmd.Run(args))

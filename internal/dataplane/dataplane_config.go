fe9f7d25c80dad8524076f02110a40971a48af01
Ganesh S <ganesh.seetharaman@hashicorp.com>
2023-06-27 10:50:31 -0700

    Added ability to generate dataplane configuration  (#145)
    * Added ability to generate dataplane configuration
    
    * Remove default configs
    
    * Modify approach to use encoding/json

---
+++ internal/dataplane/dataplane_config.go
@@ -0,0 +1,65 @@
+package dataplane
+
+import (
+	"github.com/hashicorp/consul-ecs/config"
+	"github.com/hashicorp/consul/api"
+)
+
+const (
+	localhostAddr = "127.0.0.1"
+)
+
+// GetDataplaneConfigJSONInputs are the inputs needed to
+// generate a dataplane configuration JSON
+type GetDataplaneConfigJSONInput struct {
+	// Registration details about the proxy service
+	ProxyRegistration *api.CatalogRegistration
+
+	// User provided information about the Consul servers
+	ConsulServerConfig config.ConsulServers
+
+	// ACL token returned by the server after a successful login.
+	// If empty, credential details are not populated in the resulting
+	// dataplane config JSON.
+	ConsulToken string
+}
+
+// GetDataplaneConfigJSON returns back a configuration JSON which
+// (after writing it to a shared volume) can be used to start consul-dataplane
+func (i *GetDataplaneConfigJSONInput) GetDataplaneConfigJSON() ([]byte, error) {
+	cfg := &dataplaneConfig{
+		Consul: ConsulConfig{
+			Addresses:       i.ConsulServerConfig.Hosts,
+			GRPCPort:        i.ConsulServerConfig.GRPCPort,
+			SkipServerWatch: i.ConsulServerConfig.SkipServerWatch,
+		},
+		Service: ServiceConfig{
+			NodeName:       i.ProxyRegistration.Node,
+			ProxyServiceID: i.ProxyRegistration.Service.ID,
+			Namespace:      i.ProxyRegistration.Service.Namespace,
+			Partition:      i.ProxyRegistration.Service.Partition,
+		},
+		XDSServer: XDSServerConfig{
+			Address: localhostAddr,
+			Port:    20000,
+		},
+	}
+
+	if i.ConsulServerConfig.EnableTLS {
+		cfg.Consul.TLS = &TLSConfig{
+			GRPCCACertPath: i.ConsulServerConfig.CACertFile,
+			TLSServerName:  i.ConsulServerConfig.TLSServerName,
+		}
+	}
+
+	if i.ConsulToken != "" {
+		cfg.Consul.Credentials = &CredentialsConfig{
+			CredentialType: "static",
+			Static: StaticCredentialConfig{
+				Token: i.ConsulToken,
+			},
+		}
+	}
+
+	return cfg.generateJSON()
+}

de65b9bb05d8b676e8d6cd65c3909e9402c95e0c
Abhishek <abhishek.sahu@hashicorp.com>
2024-12-09 18:27:36 +0530

    Update command_test.go

--- subcommand/health-sync/command_test.go
+++ subcommand/health-sync/command_test.go
@@ -387,7 +387,7 @@ func TestRun(t *testing.T) {
 					if c.missingDataplaneContainer {
 						expCheck.Status = api.HealthCritical
 					} else {
-						expCheck.Status = api.HealthPassing
+						expCheck.Status = api.HealthCritical
 					}
 				}
 			}

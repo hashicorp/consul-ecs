2496449ea363ec7025e483f2b6359fc214e61c21
Paras Gupta <paras.gupta@hashicorp.com>
2026-03-02 17:04:10 +0530

    Resolve the CVE's and updated the versions before backporting to release branch.  (#317)
    * Fix the CVE's and alerts from the secvuln ticket and PR
    
    * Fix golangci-lint v2.8.0 schema validation error
    
    * Add --no-cache flag to apk upgrade for better Docker build reproducibility and update changelog

--- testutil/iamauthtest/testing.go
+++ testutil/iamauthtest/testing.go
@@ -9,6 +9,7 @@ package iamauthtest
 import (
 	"encoding/xml"
 	"fmt"
+	"html"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -81,7 +82,7 @@ func writeXML(w http.ResponseWriter, val interface{}) {
 
 func writeError(w http.ResponseWriter, code int, r *http.Request) {
 	w.WriteHeader(code)
-	msg := fmt.Sprintf("%s %s", r.Method, r.URL)
+	msg := fmt.Sprintf("%s %s", html.EscapeString(r.Method), html.EscapeString(r.URL.String()))
 	if _, writeErr := fmt.Fprintf(w, `<ErrorResponse xmlns="https://fakeaws/">
   <Error>
 	<Message>Fake AWS Server Error: %s</Message>
